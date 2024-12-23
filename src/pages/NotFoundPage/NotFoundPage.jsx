edee002daa557cfddc09eae330637a5a3e3f3b77
ptvmarch26 <vovanphithong263@gmail.com>
2024-12-23 22:59:15 +0700

    update

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import styles from './NotFouldPage.module.scss'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.notFoundContainer}>
       <p className={styles.errorCode}>404</p>
-      <p className={styles.errorMessage}>Oops! The page you're looking for doesn't exist.</p>
-      <a href="/" className={styles.homeLink}>Go Back to Home</a>
+      <p className={styles.errorMessage}>Trang bạn tìm kiếm không tồn tại</p>
+      <button onClick={() => navigate(-1)} className={styles.backButton}>
+        Quay lại trang trước đó
+      </button>
     </div>
   );
-}
+};
 
-export default NotFoundPage
+export default NotFoundPage;
